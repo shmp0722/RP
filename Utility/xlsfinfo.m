348b9f8c763bb0becb44386fcb26b6cf8eefe27a
shmp0722 <shmp0722@gmail.com>
2014-10-29 14:41:32 -0700

    put life

--- Utility/xlsfinfo.m
+++
@@ -1,8 +0,0 @@
-function [a, sheets]=xlsfinfo('file.xls')
-% function [a, sheets]=xlsfinfo('filename.xls')
-
-for ind=1:length(sheets)
-    disp('---');
-    disp(sheets(ind));
-    [data,header]=xlsread('file.xls',ind)
-end;
