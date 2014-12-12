3468afddf03b8ab8c666b580d93bc1a71396b632
shmp0722 <shmp0722@gmail.com>
2014-12-12 11:42:11 -0800

    add options

--- TamagawaPath.m
+++ TamagawaPath.m
@@ -1,32 +1,40 @@
 function Path = TamagawaPath(option)
 %
 % Change directory to '/DWI-Tamagawa-Japan';
-% 
+%
 %
 %
 % SO Vista lab 2014
 
 
-%%
+%% argument check
 if isempty(option); option =1;
 end
-
+if ischar(option)
+    option = lower(strrep(option,' ',''));
+end
+%% Where is your destination?
 switch option
     case {1}
         Path = '/biac4/wandell/biac2/wandell/data/DWI-Tamagawa-Japan';
-        cd(Path)         
     case {2}
         Path = '/biac4/wandell/biac2/wandell/data/DWI-Tamagawa-Japan2';
-        cd(Path)
     case {3}
         Path = '/biac4/wandell/biac2/wandell/data/DWI-Tamagawa-Japan3';
-        cd(Path)
+    case {'subj','subject','subjectname'}
+        Path = '/biac4/wandell/biac2/wandell/data/DWI-Tama-subjects';
     case {'git'}
         Path = '/home/shumpei/matlab/git';
-        cd(Path)
     case {'freesurfer'}
         Path = '/biac4/wandell/biac2/wandell/data/DWI-Tamagawa-Japan/freesurfer';
-        cd(Path)
+    case {'qmr','qmri','quantitative'}
+        Path = '/biac4/wandell/biac2/wandell/data/qMRI';
+    case {'mouse'}
+        Path = '/biac4/wandell/biac2/wandell/data/Ogawa_Mouse_DWI';
+    case {'life'}
+end
+cd(Path)
+sprintf('You are here. "%s"',Path)
 end
 
 
