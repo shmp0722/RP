4788baf69e5ce2f5cc67c4aefc5979a7b1d64471
shmp0722 <shmp0722@gmail.com>
2015-02-12 15:29:00 -0800

    figure

--- RP_plot_24C_TP.m
+++ RP_plot_24C_TP.m
@@ -543,3 +543,274 @@ end
 %% save figures
 saveas(gcf,'MDADRD_OTOR.png')
 saveas(gcf,'MDADRD_OTOR.eps','psc2')
+
+
+
+
+
+%% AD RD plot
+mrvNewGraphWin; hold on;
+% OT
+fibID = 3; % OT
+pctl = 1;
+% take diffusion values
+for subID = 1:length(TractProfile);
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        fa(subID,:) =nan(1,100);
+    else
+        fa(subID,:) =  mean([TractProfile{subID,fibID}{pctl}.vals.fa;...
+            TractProfile{subID,fibID+1}{pctl}.vals.fa]);
+    end;
+    
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        md(subID,:) =nan(1,100);
+    else
+        md(subID,:) = mean([ TractProfile{subID,fibID}{pctl}.vals.md;...
+            TractProfile{subID,fibID+1}{pctl}.vals.md]);
+    end;
+    
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        rd(subID,:) =nan(1,100);
+    else
+        rd(subID,:) = mean([ TractProfile{subID,fibID}{pctl}.vals.rd;...
+            TractProfile{subID,fibID+1}{pctl}.vals.rd]);
+    end;
+    
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        ad(subID,:) =nan(1,100);
+    else
+        ad(subID,:) = mean([ TractProfile{subID,fibID}{pctl}.vals.ad;...
+            TractProfile{subID,fibID+1}{pctl}.vals.ad]);
+    end;
+    
+end
+
+% switch based on property
+Diffusion = {'ad','rd'};
+for ii =1:length(Diffusion)
+    property  =Diffusion{ii};
+    %% switch based on property
+    switch property
+        case {'fa','FA'}
+            % raw data
+            ValCtl =  fa(Ctl,:);
+            ValRP =  fa(RP,:);
+            
+            % for axis
+            XLIM  = [10,90];
+            YLIM  = [0.1 0.6];
+            
+            
+        case {'md','MD'}
+            ValCtl =  md(Ctl,:);
+            ValRP =  md(RP,:);
+            
+            XLIM  = [10,90];
+            YLIM  = [0.6 1.8];
+            
+            
+        case {'ad','AD'}
+            ValCtl =  ad(Ctl,:);
+            ValRP =  ad(RP,:);
+            
+            XLIM  = [10,90];
+            YLIM  = [1 2.2];
+            
+        case {'rd','RD'}
+            ValCtl =  rd(Ctl,:);
+            ValRP =  rd(RP,:);
+            
+            XLIM  = [10,90];
+            YLIM  = [0.4 1.8];
+    end
+    
+    XTICK = XLIM;
+    YTICK = YLIM;
+    XTICKLabel = {'OC','LGN'};
+    
+    % stats Wilcoxson
+    for kk = 1:length(ValRP);
+        [p(kk),h(kk)] = ranksum(ValCtl(:,kk),ValRP(:,kk),'alpha',0.01);
+        [P(kk),H(kk)] = ranksum(ValCtl(:,kk),ValRP(:,kk),'alpha',0.05);
+    end
+    
+    %% plot
+    subplot(2,2,2*ii-1); hold on;
+    
+    % put bars based on wilcoxson (p<0.01, 0.05)
+    bar(H*5,1.0,'edgecolor','none','facecolor',[0.8 0.7 0.1])
+    bar(h*5,1.0,'edgecolor','none','facecolor',[0.5 0.7 0.3])
+    
+    %% Control
+    st = nanstd(ValCtl,1);
+    m   = nanmean(ValCtl);
+    
+    A3 = area(m+2*st);
+    A1 = area(m+st);
+    A2 = area(m-st);
+    A4 = area(m-2*st);
+    
+    % set color and style
+    set(A1,'FaceColor',[0.6 0.6 0.6],'linestyle','none')
+    set(A2,'FaceColor',[0.8 0.8 0.8],'linestyle','none')
+    set(A3,'FaceColor',[0.8 0.8 0.8],'linestyle','none')
+    set(A4,'FaceColor',[1 1 1],'linestyle','none')
+    
+    % mean plot
+    plot(m,'color',[0 0 0], 'linewidth',3 )
+    
+    % RP individual
+    for k = RP
+        plot(X,ValRP,'Color',c(5,:),...
+            'linewidth',1);
+    end
+    % mean
+    plot(X,nanmean(ValRP),'Color',c(5,:),...
+        'linewidth',3);
+    % labeling
+    ylabel(upper(property),'FontName','Times','FontSize',14);
+    xlabel('Location','FontName','Times','FontSize',14);
+    set(gca, 'xlim',XLIM,'xtick',XLIM,'xtickLabel',{'LGN','V1'},...
+        'tickDir','out','ylim',YLIM,...
+        'ytick',YLIM)
+    %     axis('square')
+    
+    %     Percentile = {'0-20','20-40','40-60','60-80','80-100'};
+    title('Optic Tract','FontSize',18)
+
+    hold off;
+    
+end
+
+% Add OR
+fibID = 1; % OR
+pctl = 1;
+% take diffusion values
+for subID = 1:length(TractProfile);
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        fa(subID,:) =nan(1,100);
+    else
+        fa(subID,:) =  mean([TractProfile{subID,fibID}{pctl}.vals.fa;...
+            TractProfile{subID,fibID+1}{pctl}.vals.fa]);
+    end;
+    
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        md(subID,:) =nan(1,100);
+    else
+        md(subID,:) = mean([ TractProfile{subID,fibID}{pctl}.vals.md;...
+            TractProfile{subID,fibID+1}{pctl}.vals.md]);
+    end;
+    
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        rd(subID,:) =nan(1,100);
+    else
+        rd(subID,:) = mean([ TractProfile{subID,fibID}{pctl}.vals.rd;...
+            TractProfile{subID,fibID+1}{pctl}.vals.rd]);
+    end;
+    
+    if isempty(TractProfile{subID,fibID}{pctl}.nfibers);
+        ad(subID,:) =nan(1,100);
+    else
+        ad(subID,:) = mean([ TractProfile{subID,fibID}{pctl}.vals.ad;...
+            TractProfile{subID,fibID+1}{pctl}.vals.ad]);
+    end;
+    
+end
+
+% switch based on property
+Diffusion = {'md','ad','rd'};
+for ii =1:length(Diffusion)
+    property  =Diffusion{ii};
+    %% switch based on property
+    switch property
+        case {'fa','FA'}
+            % raw data
+            ValCtl =  fa(Ctl,:);
+            ValRP =  fa(RP,:);
+            % for axis
+            XLIM  = [10,90];
+            YLIM  = [0.2 0.8];
+            
+        case {'md','MD'}
+            ValCtl =  md(Ctl,:);
+            ValRP =  md(RP,:);
+            
+            XLIM  = [10,90];
+            YLIM  = [0.5 1.1];
+            
+        case {'ad','AD'}
+            ValCtl =  ad(Ctl,:);
+            ValRP =  ad(RP,:);
+            
+            XLIM  = [10,90];
+            YLIM  = [1 1.8];
+            
+        case {'rd','RD'}
+            ValCtl =  rd(Ctl,:);
+            ValRP =  rd(RP,:);
+            
+            XLIM  = [10,90];
+            YLIM  = [0.3 0.9];
+            
+    end
+    
+    
+    XTICKLabel = {'LGN','V1'};
+    
+    % stats Wilcoxson
+    for kk = 1:length(ValRP);
+        [p(kk),h(kk)] = ranksum(ValCtl(:,kk),ValRP(:,kk),'alpha',0.01);
+        [P(kk),H(kk)] = ranksum(ValCtl(:,kk),ValRP(:,kk),'alpha',0.05);
+    end
+    
+    %% plot
+    subplot(2,2,2*ii); hold on;
+    
+    % put bars based on wilcoxson (p<0.01, 0.05)
+    bar(H*5,1.0,'edgecolor','none','facecolor',[0.8 0.7 0.1])
+    bar(h*5,1.0,'edgecolor','none','facecolor',[0.5 0.7 0.3])
+    
+    %% Control
+    st = nanstd(ValCtl,1);
+    m   = nanmean(ValCtl);
+    
+    A3 = area(m+2*st);
+    A1 = area(m+st);
+    A2 = area(m-st);
+    A4 = area(m-2*st);
+    
+    % set color and style
+    set(A1,'FaceColor',[0.6 0.6 0.6],'linestyle','none')
+    set(A2,'FaceColor',[0.8 0.8 0.8],'linestyle','none')
+    set(A3,'FaceColor',[0.8 0.8 0.8],'linestyle','none')
+    set(A4,'FaceColor',[1 1 1],'linestyle','none')
+    
+    % mean plot
+    plot(m,'color',[0 0 0], 'linewidth',3 )
+    
+    % RP individual
+    for k = RP
+        plot(X,ValRP,'Color',c(5,:),...
+            'linewidth',1);
+    end
+    % mean
+    plot(X,nanmean(ValRP),'Color',c(5,:),...
+        'linewidth',3);
+    % labeling
+    ylabel(upper(property),'FontName','Times','FontSize',14);
+    xlabel('Location','FontName','Times','FontSize',14);
+    set(gca, 'xlim',XLIM,'xtick',XLIM,'xtickLabel',XTICKLabel,...
+        'tickDir','out','ylim',YLIM,...
+        'ytick',YLIM)
+    %     axis('square')
+    
+    %     Percentile = {'0-20','20-40','40-60','60-80','80-100'};
+    title('Optic Radiation','FontSize',18)
+    hold off;
+    
+end
+
+%% save figures
+saveas(gcf,'ADRD_OTOR.png')
+saveas(gcf,'ADRD_OTOR.eps','psc2')
+
---
+++ runAFQ_RP8N25.m
@@ -0,0 +1,36 @@
+function afq = runAFQ_RP8N25
+%% Checking if these subjects have OR and OT files
+
+[homeDir,subDir,JMD,CRD,LHON,Ctl,RP,AMDC] = Tama_subj2;
+
+% Pick up subject
+
+Control = [Ctl,AMDC];
+subnumber = [RP,Control];
+
+% AFQ
+for jj = 1: length(subnumber)
+    sub_dirs{jj} = fullfile(homeDir, subDir{subnumber(jj)},'dwi_2nd');
+end
+% Subject grouping is a little bit funny because afq only takes two groups
+% but we have 3. For now we will divide it up this way but we can do more
+% later
+a = zeros(1,length(subnumber));
+% b = [16:23,31:33,35:37];
+a(1,1:length(RP)) = 1; 
+sub_group = a;
+% sub_group = [1,0];
+
+% Now create and afq structure
+afq = AFQ_Create('sub_dirs', sub_dirs, 'sub_group', sub_group);
+% if you would like to use ants for normalization
+% afq = AFQ_Create('sub_dirs', sub_dirs, 'sub_group', sub_group, 'clip2rois', 0,'normalization','ants');
+
+afq = AFQ_set(afq,'outdir','/sni-storage/wandell/biac2/wandell/data/DWI-Tamagawa-Japan2/RP');
+afq = AFQ_set(afq,'outname','afq_8RP_25Normal_02122015');
+afq.params.run_mode = 'mrtrix';
+
+%% Run AFQ on these subjects
+
+afq = AFQ_run(sub_dirs, sub_group, afq);
+
